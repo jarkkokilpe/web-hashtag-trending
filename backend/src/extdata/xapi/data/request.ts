6f5170ab37e790bdf7eb5b2e7b99d0adad372c91
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-01-29 15:55:56 +0200

    Update the list of EXT API requests

--- backend/src/extdata/xapi/data/request.ts
+++ backend/src/extdata/xapi/data/request.ts
@@ -1,250 +1,454 @@
 export const rollingXapiWoeid = [
   {
-    country: 'Algeria',
+    area: 'Algeria',
     woeid: 23424740,
   },
   {
-    country: 'Argentina',
+    area: 'Argentina',
     woeid: 23424747,
   },
   {
-    country: 'Australia',
+    area: 'Australia',
     woeid: 23424748,
   },
   {
-    country: 'Austria',
+    area: 'Austria',
     woeid: 23424750,
   },
   {
-    country: 'Bahrain',
+    area: 'Bahrain',
     woeid: 23424753,
   },
   {
-    country: 'Belarus',
+    area: 'Belarus',
     woeid: 23424765,
   },
   {
-    country: 'Belgium',
+    area: 'Belgium',
     woeid: 23424757,
   },
   {
-    country: 'Brazil',
+    area: 'Brazil',
     woeid: 23424768,
   },
   {
-    country: 'Canada',
+    area: 'Canada',
     woeid: 23424775,
   },
   {
-    country: 'Chile',
+    area: 'Chile',
     woeid: 23424782,
   },
   {
-    country: 'Colombia',
+    area: 'Colombia',
     woeid: 23424787,
   },
   {
-    country: 'Denmark',
+    area: 'Denmark',
     woeid: 23424796,
   },
   {
-    country: 'Dominican Republic',
+    area: 'Dominican Republic',
     woeid: 23424800,
   },
   {
-    country: 'Ecuador',
+    area: 'Ecuador',
     woeid: 23424801,
   },
   {
-    country: 'Egypt',
+    area: 'Egypt',
     woeid: 23424802,
   },
   {
-    country: 'France',
+    area: 'France',
     woeid: 23424819,
   },
   {
-    country: 'Germany',
+    area: 'Germany',
     woeid: 23424829,
   },
   {
-    country: 'Ghana',
+    area: 'Ghana',
     woeid: 23424824,
   },
   {
-    country: 'Greece',
+    area: 'Greece',
     woeid: 23424833,
   },
   {
-    country: 'Guatemala',
+    area: 'Guatemala',
     woeid: 23424834,
   },
   {
-    country: 'India',
+    area: 'India',
     woeid: 23424848,
   },
   {
-    country: 'Indonesia',
+    area: 'Indonesia',
     woeid: 23424846,
   },
   {
-    country: 'Ireland',
+    area: 'Ireland',
     woeid: 23424803,
   },
   {
-    country: 'Israel',
+    area: 'Israel',
     woeid: 23424852,
   },
   {
-    country: 'Italy',
+    area: 'Italy',
     woeid: 23424853,
   },
   {
-    country: 'Japan',
+    area: 'Japan',
     woeid: 23424856,
   },
   {
-    country: 'Jordan',
+    area: 'Jordan',
     woeid: 23424860,
   },
   {
-    country: 'Kenya',
+    area: 'Kenya',
     woeid: 23424863,
   },
   {
-    country: 'Kuwait',
+    area: 'Kuwait',
     woeid: 23424870,
   },
   {
-    country: 'Latvia',
+    area: 'Latvia',
     woeid: 23424874,
   },
   {
-    country: 'Lebanon',
+    area: 'Lebanon',
     woeid: 23424873,
   },
   {
-    country: 'Malaysia',
+    area: 'Malaysia',
     woeid: 23424901,
   },
   {
-    country: 'Mexico',
+    area: 'Mexico',
     woeid: 23424900,
   },
   {
-    country: 'Netherlands',
+    area: 'Netherlands',
     woeid: 23424909,
   },
   {
-    country: 'New Zealand',
+    area: 'New Zealand',
     woeid: 23424916,
   },
   {
-    country: 'Nigeria',
+    area: 'Nigeria',
     woeid: 23424908,
   },
   {
-    country: 'Norway',
+    area: 'Norway',
     woeid: 23424910,
   },
   {
-    country: 'Oman',
+    area: 'Oman',
     woeid: 23424898,
   },
   {
-    country: 'Pakistan',
+    area: 'Pakistan',
     woeid: 23424922,
   },
   {
-    country: 'Panama',
+    area: 'Panama',
     woeid: 23424924,
   },
   {
-    country: 'Peru',
+    area: 'Peru',
     woeid: 23424919,
   },
   {
-    country: 'Philippines',
+    area: 'Philippines',
     woeid: 23424934,
   },
   {
-    country: 'Poland',
+    area: 'Poland',
     woeid: 23424923,
   },
   {
-    country: 'Portugal',
+    area: 'Portugal',
     woeid: 23424925,
   },
   {
-    country: 'Puerto Rico',
+    area: 'Puerto Rico',
     woeid: 23424935,
   },
   {
-    country: 'Qatar',
+    area: 'Qatar',
     woeid: 23424930,
   },
   {
-    country: 'Russia',
+    area: 'Russia',
     woeid: 23424936,
   },
   {
-    country: 'Saudi Arabia',
+    area: 'Saudi Arabia',
     woeid: 23424938,
   },
   {
-    country: 'Singapore',
+    area: 'Singapore',
     woeid: 23424948,
   },
   {
-    country: 'South Africa',
+    area: 'South Africa',
     woeid: 23424942,
   },
   {
-    country: 'South Korea',
+    area: 'South Korea',
     woeid: 23424868,
   },
   {
-    country: 'Spain',
+    area: 'Spain',
     woeid: 23424950,
   },
   {
-    country: 'Sweden',
+    area: 'Sweden',
     woeid: 23424954,
   },
   {
-    country: 'Switzerland',
+    area: 'Switzerland',
     woeid: 23424957,
   },
   {
-    country: 'Thailand',
+    area: 'Thailand',
     woeid: 23424960,
   },
   {
-    country: 'Turkey',
+    area: 'Turkey',
     woeid: 23424969,
   },
   {
-    country: 'Ukraine',
+    area: 'Ukraine',
     woeid: 23424976,
   },
   {
-    country: 'United Arab Emirates',
+    area: 'United Arab Emirates',
     woeid: 23424738,
   },
   {
-    country: 'United Kingdom',
+    area: 'United Kingdom',
     woeid: 23424975,
   },
   {
-    country: 'United States',
+    area: 'United States',
     woeid: 23424977,
   },
   {
-    country: 'Venezuela',
+    area: 'Venezuela',
     woeid: 23424982,
   },
   {
-    country: 'Vietnam',
+    area: 'Vietnam',
     woeid: 23424984,
+  },
+  {
+    area: 'Alabama',
+    woeid: 2347559,
+  },
+  {
+    area: 'Alaska',
+    woeid: 2347560,
+  },
+  {
+    area: 'Arizona',
+    woeid: 2347561,
+  },
+  {
+    area: 'Arkansas',
+    woeid: 2347562,
+  },
+  {
+    area: 'California',
+    woeid: 2347563,
+  },
+  {
+    area: 'Colorado',
+    woeid: 2347564,
+  },
+  {
+    area: 'Connecticut',
+    woeid: 2347565,
+  },
+  {
+    area: 'Delaware',
+    woeid: 2347566,
+  },
+  {
+    area: 'District of Columbia',
+    woeid: 2347567,
+  },
+  {
+    area: 'Florida',
+    woeid: 2347568,
+  },
+  {
+    area: 'Georgia',
+    woeid: 2347569,
+  },
+  {
+    area: 'Hawaii',
+    woeid: 2347570,
+  },
+  {
+    area: 'Idaho',
+    woeid: 2347571,
+  },
+  {
+    area: 'Illinois',
+    woeid: 2347572,
+  },
+  {
+    area: 'Indiana',
+    woeid: 2347573,
+  },
+  {
+    area: 'Iowa',
+    woeid: 2347574,
+  },
+  {
+    area: 'Kansas',
+    woeid: 2347575,
+  },
+  {
+    area: 'Kentucky',
+    woeid: 2347576,
+  },
+  {
+    area: 'Louisiana',
+    woeid: 2347577,
+  },
+  {
+    area: 'Maine',
+    woeid: 2347578,
+  },
+  {
+    area: 'Maryland',
+    woeid: 2347579,
+  },
+  {
+    area: 'Massachusetts',
+    woeid: 2347580,
+  },
+  {
+    area: 'Michigan',
+    woeid: 2347581,
+  },
+  {
+    area: 'Minnesota',
+    woeid: 2347582,
+  },
+  {
+    area: 'Mississippi',
+    woeid: 2347583,
+  },
+  {
+    area: 'Missouri',
+    woeid: 2347584,
+  },
+  {
+    area: 'Montana',
+    woeid: 2347585,
+  },
+  {
+    area: 'Nebraska',
+    woeid: 2347586,
+  },
+  {
+    area: 'Nevada',
+    woeid: 2347587,
+  },
+  {
+    area: 'New Hampshire',
+    woeid: 2347588,
+  },
+  {
+    area: 'New Jersey',
+    woeid: 2347589,
+  },
+  {
+    area: 'New Mexico',
+    woeid: 2347590,
+  },
+  {
+    area: 'New York',
+    woeid: 2347591,
+  },
+  {
+    area: 'North Carolina',
+    woeid: 2347592,
+  },
+  {
+    area: 'North Dakota',
+    woeid: 2347593,
+  },
+  {
+    area: 'Ohio',
+    woeid: 2347594,
+  },
+  {
+    area: 'Oklahoma',
+    woeid: 2347595,
+  },
+  {
+    area: 'Oregon',
+    woeid: 2347596,
+  },
+  {
+    area: 'Pennsylvania',
+    woeid: 2347597,
+  },
+  {
+    area: 'Rhode Island',
+    woeid: 2347598,
+  },
+  {
+    area: 'South Carolina',
+    woeid: 2347599,
+  },
+  {
+    area: 'South Dakota',
+    woeid: 2347600,
+  },
+  {
+    area: 'Tennessee',
+    woeid: 2347601,
+  },
+  {
+    area: 'Texas',
+    woeid: 2347602,
+  },
+  {
+    area: 'Utah',
+    woeid: 2347603,
+  },
+  {
+    area: 'Vermont',
+    woeid: 2347604,
+  },
+  {
+    area: 'Virginia',
+    woeid: 2347605,
+  },
+  {
+    area: 'Washington',
+    woeid: 2347606,
+  },
+  {
+    area: 'West Virginia',
+    woeid: 2347607,
+  },
+  {
+    area: 'Wisconsin',
+    woeid: 2347608,
+  },
+  {
+    area: 'Wyoming',
+    woeid: 2347609,
   },
 ];
