033e26e0666bc6fd13c86b9297dc11d70c452e74
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-03-01 13:08:16 +0200

    Clean up

--- backend/src/_utils/constants.ts
+++ backend/src/_utils/constants.ts
@@ -3,4 +3,3 @@ export const XAPI_RATE_LIMIT = 75; // 75 GETs
 export const XAPI_RATE_LIMIT_WINDOW_MS = 15 * 60 * 1000; // 15 minutes in milliseconds
 
 export const XAPI_IN_USE = false;
-//export const FB_API_IN_USE = false;
