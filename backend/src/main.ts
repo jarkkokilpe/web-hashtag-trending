219afe3a8837e8e04135f1f10278f537a9114432
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-03-26 21:13:41 +0200

    Fix CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS
+  const corsOrigins = (process.env.CORS_ORIGINS ?? '')
+    .split(',')
+    .map((origin) => origin.trim());
+
+  app.enableCors({
+    origin: corsOrigins, // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Enable if using cookies/auth headers
+  });
   app.use(compression());
   await app.listen(process.env.API_PORT ?? 4000);
 }
