2aeac503a26d3d03eaeb872820b69e27c4822779
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-03-29 13:24:06 +0200

    Fix workflow

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'react-app',
   transformIgnorePatterns: [
-    '/node_modules/(?!d3|d3-array|d3-geo)/', // Transform d3 and its submodules
+    '/node_modules/(?!d3|d3-array|d3-geo|d3-scale|d3-shape)/', // Include d3 and its submodules
   ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Use babel-jest to transform files
+  },
+  testEnvironment: 'jsdom', // Ensure the test environment is set to jsdom for React
 };
